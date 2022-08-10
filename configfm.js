0d3735c8aaf29ec4739422f733a8ea16b3cf5922
DeathGM <51689465+DeathGM@users.noreply.github.com>
2022-08-11 00:06:15 +0700

    1

---
+++ configfm.js
@@ -0,0 +1,24 @@
+let config = {
+   room1: {
+     autoStart: true,
+     roomName: `🏆 Thế Vận Hội 1 - Đấu Kiếm - Fencing 🏆`,
+     noPlayer: true,
+     maxPlayers: 3,
+     public: true,
+     geo: {code: "VN", lat: 22.63333, lon: 106.4},
+     roomScript: "/home/hqd_wendal/tvh-fm/fencing.js",
+   },
+   room2: {
+     autoStart: true,
+     roomName: `🏆 Thế Vận Hội 1 - Đấu Vật - MMA 🏆`,
+     noPlayer: true,
+     maxPlayers: 3,
+     public: true,
+     geo: {code: "VN", lat: 21.02888, lon: 105.85464},
+     roomScript: "/home/hqd_wendal/tvh-fm/mma.js",
+   },
+
+
+ };
+ module.exports = config;
+ 
