3cd764d52c26978a5555b16c6ffe8b42c8da37af
QuangDung-Wenal <105780006+QuangDung-Wenal@users.noreply.github.com>
2022-08-25 05:28:18 +0700

    1

--- configfm.js
+++ configfm.js
@@ -1,21 +1,21 @@
 let config = {
    room1: {
      autoStart: true,
-     roomName: `🏆 Thế Vận Hội - Đấu Kiếm - ⚔⚔ 🏆`,
+     roomName: `🏆 TVH - Đấu Kiếm - ⚔⚔ 🏆`,
 	 
      noPlayer: true,
      maxPlayers: 3,
      public: true,
-     geo: {code: "VN", lat: 21.02888, lon: 105.85464},
+      geo: {code: "VN", lat: 11.9360, lon: 108.4172},
      roomScript: "/home/hqd_wendal/tvh-fm/fencing.js",
    },
    room2: {
      autoStart: true,
-     roomName: `🏆 Thế Vận Hội - Đấu Vật - 🥊🥊 🏆`,
+     roomName: `🏆 TVH - Đấu Vật - 🥊🥊 🏆`,
      noPlayer: true,
      maxPlayers: 3,
      public: true,
-     geo: {code: "VN", lat: 21.02888, lon: 105.85464},
+      geo: {code: "VN", lat: 11.9359, lon: 108.4172},
      roomScript: "/home/hqd_wendal/tvh-fm/mma.js",
    },
 
